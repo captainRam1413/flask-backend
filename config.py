396b3cf265ffa823e555fa08e886289535e2574f
captainRam1413 <captian.ram1413@gmail.com>
2024-06-11 20:00:48 +0530

    first Commit

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(seconds=30)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=30)
 
 db = SQLAlchemy(app)
 
