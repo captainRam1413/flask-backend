9f5ec8189a5d4570d70e0eb7613a5c112f0c5ba3
captainRam1413 <captian.ram1413@gmail.com>
2024-06-18 00:08:26 +0530

    code-change commit

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from auth import auth
 
 app.register_blueprint(auth)
 
+@app.route('/')
+def home():
+    return "<h1>Home</h1>"
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
